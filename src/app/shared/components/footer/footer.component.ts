835590a3d31f2685b4dcef69f3bed0234bd4d52c
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2023-01-11 12:00:18 +0000

    feat: footer design and functionality

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,39 @@
 /* Angular */
 import { Component } from '@angular/core';
 
+/* NgRx */
+import { Store } from "@ngrx/store";
+import { navigateTo } from "../../../core/state/navigation/navigation.actions";
+
+/* Enums */
+import { SectionEnum } from "../../enums/section.enum";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+
+  public sectionTypes: typeof SectionEnum = SectionEnum;
+
+  constructor(
+    private store: Store
+  ) { }
+
+  public onClickNavigateTo(section: SectionEnum): void {
+    this.dispatchNavigateTo(section);
+  }
+
+  public navigateByUrl(url: string): void {
+    window.open(url, '_blank');
+  }
+
+
+  /* ----- Store Dispatchers ------------------------------------------------------------------------------------------------------------ */
+
+  private dispatchNavigateTo(section: SectionEnum): void {
+    this.store.dispatch(navigateTo({ section }));
+  }
+
+}
